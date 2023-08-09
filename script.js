a1ebe6077739ac6ff75d691a22e6797d7ac3a691
Jitender Kumar <111197506+Jitender72658@users.noreply.github.com>
2023-08-09 09:15:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //your code here
 
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+let revmovedArticles = [];
+for(let i = 0;i<touristSpots.length;i++){
+	if(touristSpots[i].substring(0,2)=='A '){
+	revmovedArticles[i] = touristSpots[i].substring(2);
+		}
+	else if(touristSpots[i].substring(0,2)=='An'){
+	revmovedArticles[i] = touristSpots[i].substring(3);
+		}
+	else if(touristSpots[i].substring(0,3)=='The'){
+	revmovedArticles[i] = touristSpots[i].substring(4);
+		}
+	else{
+	revmovedArticles[i] = touristSpots[i];
+	}
+}
+revmovedArticles.sort((a,b)=>a.localeCompare(b));
+for(let i =0;i<revmovedArticles.length;i++){
+	touristSpots[i] = revmovedArticles[i];
+}
